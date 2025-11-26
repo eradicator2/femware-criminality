55db57ad4bd32cdd826dedb18a3596e9a86b5314
eradicator2 <mvdstavropolck.ru@gmail.com>
2025-11-26 14:12:30 +0500

    Create verify.lua

---
+++ verify.lua
@@ -0,0 +1,40 @@
+-- ts file was generated at discord.gg/25ms
+
+
+HWList = {
+    ["9B77E61B-4451-409E-BA92-6A3B17A83564"] = true,
+    ["8CFBF123-320F-4B6D-9F1B-91793FC79010"] = true,
+    ["B9E08D7D-92E0-4CF0-9591-40354523DC45"] = true,
+    ["771BA5AD-C8E5-4A1F-81CA-B6C4BAFCE9C1"] = true,
+    ["41E8C651-71F7-4CD1-92C7-710EE90C37B3"] = true,
+    ["A645C7E7-0C72-4F9E-912F-57FC478E9E82"] = true,
+    ["2E303732-C2F4-49EB-A78C-92F5FED9D07A"] = true,
+    ["B7D79F1D-D086-4486-9107-A9E58D09DB7A"] = true,
+    ["4A5DB404-842F-43AE-A1F6-41DCDD1C7F0E"] = true,
+    ["DF11EE68-BFED-4D2D-9CF7-2615049644A6"] = true,
+    ["2B5FB27E-D36B-44E7-89F2-142CCF1EC6B3"] = true,
+    ["A30B6C6D-E698-4E60-9516-5BE3D67AD1AD"] = true,
+    ["88890979-908C-4290-BDE1-F002107FB817"] = true,
+    ["BF15BD88-78BF-43AD-89DF-ED6661545D12"] = true
+}
+WLID = {
+    [3840410914] = true,
+    [561770655] = true,
+    [414436563] = true,
+    [4071032541] = true,
+    [284746562] = true,
+    [4315525526] = true,
+    [4166436780] = true,
+    [418347542] = true,
+    [1351868] = true,
+    [462926952] = true,
+    [2463006608] = true,
+    [160859907] = true,
+    [2640163466] = true,
+    [3687958726] = true,
+    [2573880103] = true,
+    [547484807] = true,
+    [4293998201] = true,
+    [2697481077] = true,
+    [2054167104] = true
+}
